1364b2bfce34dfc8bbbe03a6928906a5e3417855
Aman Sultan Baig <amansultanbaig@gmail.com>
2024-10-24 14:40:46 +0500

    authenticate routes

--- src/income/income.controller.ts
+++ src/income/income.controller.ts
@@ -1,16 +1,30 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards, Req } from '@nestjs/common';
 import { IncomeService } from './income.service';
-// import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { Income } from './schemas/income.schema';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
 @ApiTags('Incomes')
 @Controller('incomes')
 export class IncomeController {
   constructor(private readonly incomeService: IncomeService) {}
 
-//   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(AuthGuard('jwt'))
   @Post()
+  @ApiOperation({ summary: 'Create a new income' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        amount: { type: 'number' },
+        categoryId: { type: 'string' },
+        sourceId: { type: 'string' },
+        description: { type: 'string' },
+      },
+    },
+  })  
+  @ApiResponse({ status: 201, description: 'Income created successfully.' })
   async createIncome(
     @Body('amount') amount: number,
     @Body('categoryId') categoryId: any,
@@ -22,20 +36,23 @@ export class IncomeController {
     return this.incomeService.createIncome(amount, categoryId, sourceId, description, userId);
   }
 
-//   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   async getAllIncomes(@Req() req: any): Promise<Income[]> {
     const userId = req.user._id;
     return this.incomeService.getAllIncomes(userId);
   }
 
-//   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
   async getIncomeById(@Param('id') id: string): Promise<Income> {
     return this.incomeService.getIncomeById(id);
   }
 
-//   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(AuthGuard('jwt'))
   @Put(':id')
   async updateIncome(
     @Param('id') id: string,
@@ -44,7 +61,8 @@ export class IncomeController {
     return this.incomeService.updateIncome(id, updateData);
   }
 
-//   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   async deleteIncome(@Param('id') id: string): Promise<void> {
     return this.incomeService.deleteIncome(id);
