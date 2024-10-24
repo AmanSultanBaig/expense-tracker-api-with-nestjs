dfbaa579db7a23b9c27bec4254769c7d19f2de77
Aman Sultan Baig <amansultanbaig@gmail.com>
2024-10-24 15:14:23 +0500

    schema defined for swagger docs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,18 +2,42 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Login' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        username: { type: 'string' },
+        password: { type: 'string' }
+      },
+    },
+  })  
+  @ApiResponse({ status: 201, description: 'logged in successfully.' })
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 
+  @ApiOperation({ summary: 'Register' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        email: { type: 'string' },
+        username: { type: 'string' },
+        password: { type: 'string' },
+        confirmPassword: { type: 'string' }
+      },
+    },
+  })  
+  @ApiResponse({ status: 201, description: 'user registered successfully.' })
   @Post('register')
   async register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
